6e102b7f46803a5dce4c66206678e0e05a7a8d29
Pedro <pedro@mail.com>
2026-09-04 21:15:19 -0300

    DAO

--- src/main/java/br/cefetrj/appcorp/controller/PessoaServlet.java
+++ src/main/java/br/cefetrj/appcorp/controller/PessoaServlet.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javax.sql.DataSource;
 
 import br.cefetrj.appcorp.model.Pessoa;
+import br.cefetrj.appcorp.service.PessoaService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -20,6 +21,14 @@ public class PessoaServlet extends HttpServlet{
         DataSource dataSource =
             (DataSource) getServletContext()
                 .getAttribute("dataSource");
+
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome(req.getParameter("nome"));
+        pessoa.setDataNascimento(
+            java.time.LocalDate.parse(req.getParameter("dataNascimento"))
+        );
+        PessoaService pessoaService = new PessoaService(dataSource);
+        pessoaService.create(pessoa);
         super.doPost(req, resp);
     }
 
--- src/main/java/br/cefetrj/appcorp/service/PessoaService.java
+++ src/main/java/br/cefetrj/appcorp/service/PessoaService.java
@@ -1,11 +1,13 @@
 package br.cefetrj.appcorp.service;
 
+import javax.sql.DataSource;
+
 import br.cefetrj.appcorp.dao.PessoaDAO;
 import br.cefetrj.appcorp.model.Pessoa;
 
 public class PessoaService extends GenericService<Pessoa>{
     
-    public PessoaService() {
-        super(new PessoaDAO());
+    public PessoaService(DataSource dataSource) {
+        super(new PessoaDAO(dataSource));
     }
 }
