3c79c15a844596a2709392c34a0d1bb030417b56
Vitor <vitorpadilha@yahoo.com.br>
2026-09-25 19:29:18 -0300

    fix: Crud Pessoa

--- src/main/java/br/cefetrj/appcorp/config/ApplicationListener.java
+++ src/main/java/br/cefetrj/appcorp/config/ApplicationListener.java
@@ -5,20 +5,34 @@ import jakarta.servlet.annotation.WebListener;
 
 import javax.sql.DataSource;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 @WebListener
 public class ApplicationListener implements ServletContextListener {
+
+    private static final Logger logger = LogManager.getLogger(ApplicationListener.class);
 
     private DatabaseConfig databaseConfig;
 
     @Override
     public void contextInitialized(ServletContextEvent event) {
+        logger.info("Inicializando conexão com o banco de dados...");
 
-        databaseConfig = new DatabaseConfig();
+        try {
+            databaseConfig = new DatabaseConfig();
+        } catch (RuntimeException e) {
+            logger.error("Erro ao conectar no banco de dados: {}", rootCauseMessage(e), e);
+            // Relança para o Tomcat interromper o deploy da aplicação
+            throw e;
+        }
 
         DataSource dataSource = databaseConfig.getDataSource();
 
         event.getServletContext()
              .setAttribute("dataSource", dataSource);
+
+        logger.info("Conexão com o banco de dados estabelecida com sucesso.");
     }
 
     @Override
@@ -26,5 +40,13 @@ public class ApplicationListener implements ServletContextListener {
         if(databaseConfig != null) {
             databaseConfig.close();
         }
+    }
+
+    private static String rootCauseMessage(Throwable e) {
+        Throwable cause = e;
+        while (cause.getCause() != null) {
+            cause = cause.getCause();
+        }
+        return cause.getClass().getSimpleName() + ": " + cause.getMessage();
     }
 }
